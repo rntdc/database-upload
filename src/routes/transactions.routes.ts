6915fcd4c3c85ad13ccc484b047084a2a9299cb5
Renato <rntdek@gmail.com>
2020-11-24 20:23:13 -0300

    importacao csv: desafio finalizado

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+import UploadConfig from '../config/upload';
+
+const upload = multer(UploadConfig);
 
 const transactionsRouter = Router();
 
@@ -42,8 +46,14 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+transactionsRouter.post('/import',
+upload.single('file'),
+async (request, response) => {
+  const importTransactions = new ImportTransactionsService();
+
+  const transactions = await importTransactions.execute(request.file.path);
+
+  return response.json(transactions);
 });
 
 export default transactionsRouter;
